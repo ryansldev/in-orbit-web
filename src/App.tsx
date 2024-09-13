d8beb587fd24bfa6a5d6e4b4e34fe5955272e529
ryansldev <ryanlima1309@gmail.com>
2024-09-13 11:31:34 -0300

    Aula 03

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { EmptyGoals } from './components/empty-goals'
 import { Summary } from './components/summary'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
 
 function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60, // 60 seconds
+  })
+
   return (
     <Dialog>
-      <Summary />
-      {/* <EmptyGoals /> */}
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
 
       <CreateGoal />
     </Dialog>
